cdae79fdad77266b9832b67c8084be0f6e770f2f
Roger <rogerlais@gmail.com>
2019-12-20 21:04:21 -0300

    Entrega final com setup.bat para startup do servidor

--- rebuild_static.bat
+++ rebuild_static.bat
@@ -1,4 +1,11 @@
 call .\djenv\scripts\activate.bat
-python manage.py collectstatic
+python manage.py collectstatic --no-input
+if %ERRORLEVEL% neq 0 goto :HAS_ERROR
+@echo "Recursos estaticos atualizados com sucesso"
+goto :END
 
+:HAS_ERROR
+echo "Problema ocorrido"
+
+:END
 pause
--- setup.bat
+++ setup.bat
@@ -1,4 +1,4 @@
-rem @echo off
+@echo off
 set predir=%cd%
 @echo %predir%
 @echo "Preparando o ambiente Python(Windows)..."
@@ -10,13 +10,14 @@ virtualenv .
 if %ERRORLEVEL% neq 0 goto :HAS_ERROR
 @echo "Ambiente criado com sucesso"
 @echo "parada venv"
-pause 
 
 :DATABASE_NEW
 if exist %predir%\DB\QFactoryDB\db.sqlite3 goto :DATABASE_OLD
-goto :ACTIVE_ENV
+git clone https://github.com/rogerlais/QFactoryDB.git DB\QFactoryDB
+goto :UPDATE_DB
 
 :DATABASE_OLD
+@echo "Atualizando a base de dados com repo remoto..."
 cd %predir%\DB\QFactoryDB
 git pull --force
 if %ERRORLEVEL% neq 0 (
@@ -31,19 +32,24 @@ copy %predir%\DB\QFactoryDB\db.sqlite3 %predir%\db.sqlite3 /y
 if %ERRORLEVEL% neq 0 goto :HAS_ERROR
 @echo "Base de dados atualizado com sucesso"
 
-
 :ACTIVE_ENV
 cd %predir%
 call %predir%\djenv\scripts\activate.bat
 if %ERRORLEVEL% neq 0 goto :HAS_ERROR
 @echo "Ambiente ativado com sucesso, instalando depedencias"
 
+
 :DEPENDENCIES
-pause
 pip install -r requirements.txt 
 if %ERRORLEVEL% neq 0 goto :HAS_ERROR
 @echo "Requisitos atualizados com sucesso"
 
+:REBUILD_STATICS
+call rebuild_static.bat
+if %ERRORLEVEL% neq 0 goto :HAS_ERROR
+
+:RUN_SERVER
+python manage.py runserver
 
 goto :END
 
